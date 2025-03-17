48cdaabacf20a738874dc5596de86eca8b9982b6
ManuelFijan <manuel.fijan@fer.unizg.hr>
2025-03-17 19:04:33 +0100

    initial commit

---
+++ src/test/java/com/OfferMaster/OfferMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OfferMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
