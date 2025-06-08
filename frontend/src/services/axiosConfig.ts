7dfd666e3e30bd2cb76e33d69af3823ca09d065c
Manuel Fijan <manuel.fijan@fer.unizg.hr>
2025-06-08 21:48:29 +0200

    dodavanje urla

--- frontend/src/services/axiosConfig.ts
+++ frontend/src/services/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {clearToken, getToken} from "./AuthSession";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
     headers: { "Content-Type": "application/json" },
 });
 
